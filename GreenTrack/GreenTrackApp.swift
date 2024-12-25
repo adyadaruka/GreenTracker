05f42caa0738539eefe9ec2f429f6c8967c008d0
adyadaruka <adaruka2@illinois.edu>
2024-12-25 22:54:08 +0100

    Initial Commit

---
+++ GreenTrack/GreenTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenTrackApp.swift
+//  GreenTrack
+//
+//  Created by Adya Daruka on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct GreenTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenTrackTests/GreenTrackTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenTrackTests.swift
+//  GreenTrackTests
+//
+//  Created by Adya Daruka on 25/12/24.
+//
+
+import Testing
+@testable import GreenTrack
+
+struct GreenTrackTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
